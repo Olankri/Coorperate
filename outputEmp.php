a7bf0c328bc500f58a681f96572f12153c9b11f8
Olankri <75421858+Olankri@users.noreply.github.com>
2020-12-05 18:05:49 +0700

    new file

---
+++ outputEmp.php
@@ -0,0 +1,20 @@
+<?php
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $dbname="project";
+    $conn = mysqli_connect($host,$user,$pass,$dbname) or die("ไม่สามารถเชื่อมต่อฐานข้อมูลได้");
+    mysqli_set_charset($conn,"utf8");
+    
+    $sql = "INSERT INTO Contacts (empID,empFullname,empNickname,empPhone,empEmail,empDepartment,empMajor,empStartDate)
+    VALUES ('{$_POST['empID']}','{$_POST['empFullName']}','{$_POST['empNickName']}','{$_POST['empPhone']}','{$_POST['empEmail']}','{$_POST['empDepartment']}','{$_POST['empMajor']}','{$_POST['empStartDate']}')";
+    
+    if (mysqli_query($conn, $sql)) {
+    $result = "บันทึกข้อมูลสำเร็จ";
+    } else {
+    $result ="Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    echo ($result);
+    mysqli_close($conn);
+    
+    ?>
